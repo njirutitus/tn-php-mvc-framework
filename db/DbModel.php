0c7faaa7fc92e2cd799b6b6fc0f1b0eeb8620125
titus njiru <njirutitus@gmail.com>
2021-04-11 15:01:47 +0300

    making primaryKey function static

--- db/DbModel.php
+++ db/DbModel.php
@@ -13,7 +13,7 @@ abstract class DbModel extends Model
 
     abstract  public function attributes(): array;
 
-    abstract public function primaryKey(): string;
+    abstract public static function primaryKey(): string;
 
     public function save() {
         $tableName = $this->tableName();
