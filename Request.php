a649e5849a9a2b1f9ef58c0ded2e42743e209f6b
titus njiru <njirutitus@gmail.com>
2021-07-18 02:48:26 +0300

    adding option to load files dat from submitted form

--- Request.php
+++ Request.php
@@ -32,17 +32,25 @@ class Request
         return $this->method() === 'post';
     }
 
+    public function isDelete()
+    {
+        return $this->method() === 'delete';
+    }
+
     public function getBody()
     {
         $body = [];
         if ($this->method() === 'get') {
             foreach ($_GET as $key => $value ) {
-                $body[$key] = filter_var(INPUT_GET,$key,FILTER_SANITIZE_SPECIAL_CHARS);
+                $body[$key] = filter_input(INPUT_GET,$key,FILTER_SANITIZE_SPECIAL_CHARS);
             }
         }
         if ($this->method() === 'post') {
             foreach ($_POST as $key => $value ) {
                 $body[$key] = filter_input(INPUT_POST,$key,FILTER_SANITIZE_SPECIAL_CHARS);
+            }
+            foreach($_FILES as $key => $value) {
+                $body[$key] = $value;
             }
         }
 
