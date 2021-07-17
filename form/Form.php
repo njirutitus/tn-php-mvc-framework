910b8dd59e18891697d155ff3d0a9b1588f26af4
titus njiru <njirutitus@gmail.com>
2021-07-18 02:52:06 +0300

    adding encytype attribute to opening form tag to allow file upload

--- form/Form.php
+++ form/Form.php
@@ -10,7 +10,7 @@ class Form
 {
     public static function begin($action, $method)
     {
-        echo sprintf('<form action="%s" method="%s">',$action, $method);
+        echo sprintf('<form action="%s" method="%s" enctype="multipart/form-data">',$action, $method);
         return new Form();
     }
 
